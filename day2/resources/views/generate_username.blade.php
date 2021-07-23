910060ecddcde0b976e374cf129fb237bf7adbd2
Desrina Putri <desrinaputri2012@gmail.com>
2021-07-23 17:11:56 +0700

    Update generate_username

--- day2/resources/views/generate_username.blade.php
+++ day2/resources/views/generate_username.blade.php
@@ -1,6 +1,10 @@
 <?php
     $email  = file_get_contents("/Users/desrinaputri/Documents/TrainingLaravel/resources/views/emails.json");
-
-    $username = strstr($email, '@', true);
-    echo $username."\n";
+    $json_array = json_decode($email, true);
+    for ( $i=0; $i < count(  $json_array ); $i++ ) 
+          { 
+            $list = $json_array[$i]['email'] . PHP_EOL;
+            $username2 = strstr($list, '@', true);
+            echo $username2."\n";                
+          }
 ?>
